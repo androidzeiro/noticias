f19e453c36d68e6145472a64c196b07c04b27c8f
Raphael Marques <raphael.marques@nuvem.net>
2020-05-01 19:48:48 -0400

    Implementando Detalhes

--- app/src/main/java/br/com/raphael/noticias/ui/DetalhesFragment.kt
+++ app/src/main/java/br/com/raphael/noticias/ui/DetalhesFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import br.com.raphael.noticias.R
 import br.com.raphael.noticias.viewmodel.DetalhesViewModel
+import cn.pedant.SweetAlert.SweetAlertDialog
 
 class DetalhesFragment : Fragment() {
 
@@ -22,6 +24,32 @@ class DetalhesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observerSuccess()
+        observerError()
+        observerLoading()
+    }
 
+    private fun observerSuccess() {
+        viewModel.success.observe(viewLifecycleOwner, Observer {
+
+        })
+    }
+
+    private fun observerError() {
+        viewModel.error.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                SweetAlertDialog(requireContext(), SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText(getString(R.string.atencao))
+                    .setContentText(it)
+                    .setConfirmText(getString(R.string.ok))
+                    .show()
+            }
+        })
+    }
+
+    private fun observerLoading() {
+        viewModel.loading.observe(viewLifecycleOwner, Observer {
+            //pb_carregando.isVisible = it
+        })
     }
 }
--- app/src/main/java/br/com/raphael/noticias/viewmodel/DetalhesViewModel.kt
+++ app/src/main/java/br/com/raphael/noticias/viewmodel/DetalhesViewModel.kt
@@ -2,6 +2,45 @@ package br.com.raphael.noticias.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import br.com.raphael.noticias.App
+import br.com.raphael.noticias.model.DocumentoDetalhes
+import br.com.raphael.noticias.repository.BackendRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class DetalhesViewModel(application: Application) : AndroidViewModel(application) {
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = _error
+
+    private val _success = MutableLiveData<DocumentoDetalhes>()
+    val success: MutableLiveData<DocumentoDetalhes>
+        get() = _success
+
+    private val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean>
+        get() = _loading
+
+    @Inject
+    lateinit var backendRepository: BackendRepository
+
+    init {
+        getApplication<App>().component.inject(this)
+    }
+
+    fun getDocumento(id: String) {
+        viewModelScope.launch {
+            try {
+                _loading.postValue(true)
+                val response = backendRepository.getDocumentoAsync(id = id)
+                _success.postValue(response.detalhes)
+                _loading.postValue(false)
+            } catch (e: Exception) {
+                _error.postValue(e.toString())
+            }
+        }
+    }
 }
