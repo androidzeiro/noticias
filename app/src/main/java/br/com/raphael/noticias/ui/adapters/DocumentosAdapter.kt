ca367282a31cd30532fd4bf24d73794b04fac08e
Raphael Marques <raphael.marques@nuvem.net>
2020-05-01 18:27:16 -0400

    Adapter para a lista de Noticías

---
+++ app/src/main/java/br/com/raphael/noticias/ui/adapters/DocumentosAdapter.kt
@@ -0,0 +1,51 @@
+package br.com.raphael.noticias.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import br.com.raphael.noticias.R
+import br.com.raphael.noticias.model.Documento
+import coil.api.load
+import coil.size.Scale
+import kotlinx.android.synthetic.main.item_noticia.view.*
+
+class DocumentosAdapter(
+    var items: MutableList<Documento>,
+    private val onDocumentoClicked: (Documento) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_noticia, parent, false)
+        return DocumentosViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is DocumentosViewHolder) {
+            val item = items[position]
+            if (item.imagem.isNotEmpty()) {
+                holder.imagem.load(item.imagem) {
+                    crossfade(750)
+                    placeholder(R.drawable.ic_image_placeholder)
+                    error(R.drawable.ic_image_placeholder)
+                    scale(Scale.FILL)
+                }
+            }
+
+            holder.container.setOnClickListener {
+                onDocumentoClicked.invoke(item)
+            }
+
+        }
+    }
+
+    private class DocumentosViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val container: ConstraintLayout by lazy { itemView.cl_container }
+        val imagem: ImageView by lazy { itemView.iv_capa }
+    }
+}
