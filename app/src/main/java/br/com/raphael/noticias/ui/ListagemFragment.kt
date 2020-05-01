876693578892ce94e0f60f879ee4c019d5ba9f94
Raphael Marques <raphael.marques@nuvem.net>
2020-05-01 19:49:00 -0400

    Implementando Listagem

--- app/src/main/java/br/com/raphael/noticias/ui/ListagemFragment.kt
+++ app/src/main/java/br/com/raphael/noticias/ui/ListagemFragment.kt
@@ -6,11 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import br.com.raphael.noticias.R
+import br.com.raphael.noticias.model.Documento
+import br.com.raphael.noticias.ui.adapters.DocumentosAdapter
 import br.com.raphael.noticias.viewmodel.ListagemViewModel
+import cn.pedant.SweetAlert.SweetAlertDialog
+import kotlinx.android.synthetic.main.fragment_listagem.*
 
 class ListagemFragment : Fragment() {
+
+    private val adapter by lazy {
+        DocumentosAdapter(
+            mutableListOf()
+        ) { onDocumentoClicked(it) }
+    }
 
     private val viewModel: ListagemViewModel by viewModels()
 
@@ -24,6 +36,51 @@ class ListagemFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupList()
+
+        observerSuccess()
+        observerError()
+        observerLoading()
+    }
+
+    private fun onDocumentoClicked(item: Documento) {
+//        val action = ListagemHeroisFragmentDirections.listagemHeroisToDetalhesHerois(id = item.id)
+//        view?.findNavController()?.navigate(action)
+    }
+
+    private fun setupList(){
+        rv_listagem.apply {
+            rv_listagem.apply {
+                layoutManager = LinearLayoutManager(this@ListagemFragment.activity, LinearLayoutManager.VERTICAL,false)
+                adapter = this@ListagemFragment.adapter
+            }
+        }
+    }
+
+    private fun observerSuccess() {
+        viewModel.success.observe(viewLifecycleOwner, Observer {
+                adapter.items.clear()
+                adapter.items.addAll(it?: mutableListOf())
+                adapter.notifyDataSetChanged()
+        })
+    }
+
+    private fun observerError() {
+        viewModel.error.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                SweetAlertDialog(requireContext(), SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText(getString(R.string.atencao))
+                    .setContentText(it)
+                    .setConfirmText(getString(R.string.ok))
+                    .show()
+            }
+        })
+    }
+
+    private fun observerLoading() {
+        viewModel.loading.observe(viewLifecycleOwner, Observer {
+            //pb_carregando.isVisible = it
+        })
     }
 
 }
--- app/src/main/java/br/com/raphael/noticias/viewmodel/ListagemViewModel.kt
+++ app/src/main/java/br/com/raphael/noticias/viewmodel/ListagemViewModel.kt
@@ -2,6 +2,46 @@ package br.com.raphael.noticias.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import br.com.raphael.noticias.App
+import br.com.raphael.noticias.model.Documento
+import br.com.raphael.noticias.repository.BackendRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class ListagemViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = _error
+
+    private val _success = MutableLiveData<List<Documento>>()
+    val success: MutableLiveData<List<Documento>>
+        get() = _success
+
+    private val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean>
+        get() = _loading
+
+    @Inject
+    lateinit var backendRepository: BackendRepository
+
+    init {
+        getApplication<App>().component.inject(this)
+    }
+
+    fun getDocumentos() {
+        viewModelScope.launch {
+            try {
+                _loading.postValue(true)
+                val response = backendRepository.getDocumentosAsync()
+                _success.postValue(response)
+                _loading.postValue(false)
+            } catch (e: Exception) {
+                _error.postValue(e.toString())
+            }
+        }
+    }
 }
