3faedb73e31d892dc7de0ef9eec144ddcc8909a2
Raphael Marques <raphael.marques@nuvem.net>
2020-05-01 20:01:21 -0400

    Implementando navegação (navigation)

--- app/src/main/java/br/com/raphael/noticias/MainActivity.kt
+++ app/src/main/java/br/com/raphael/noticias/MainActivity.kt
@@ -2,11 +2,15 @@ package br.com.raphael.noticias
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findNavController(R.id.navHost)
+            .setGraph(R.navigation.nav_graph)
     }
 }
